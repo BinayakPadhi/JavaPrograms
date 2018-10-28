ae0f0aabaf87fc857338654683db972789d70a63
BinayakPadhi <binayaksipu@gmail.com>
2018-10-28 21:34:42 +0530

    Binayak

--- ReadTextFile.java
+++ ReadTextFile.java
@@ -14,9 +14,10 @@ public class ReadTextFile {
 			
 			System.out.println("Total file size to read (in bytes) : "+ fis.available());
 			int content;
-			while ((content = fis.read()) != -1) {
+			while ((content = fis.read()) != 1) {
 				// convert to char and display it
 				System.out.print((char) content);
+				
 			}
 			
 		}catch (IOException e) {
