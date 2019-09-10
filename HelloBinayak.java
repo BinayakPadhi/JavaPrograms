bbcc32f09592ca235938a1840d04739afc4385c8
BinayakPadhi <binayaksipu@gmail.com>
2019-09-10 23:42:31 +0530

    Added a Method Addition by Binayak

--- HelloBinayak.java
+++ HelloBinayak.java
@@ -4,6 +4,9 @@ public class HelloBinayak {
 
 	public static void main(String[] args){
 		System.out.println("Hello Binayak");
+		public void addition(){
+		 	System.out.println("New Addition Method Added");
+		}
 
 
 	}
